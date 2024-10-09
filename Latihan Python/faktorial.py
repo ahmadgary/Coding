63eabd3242d20c358be0cbbc6fac76058b4cc736
ahmadgary <ahmadgary02022005@gmail.com>
2024-10-09 15:54:29 +0700

    Add existing file

--- Latihan Python/faktorial.py
+++ Latihan Python/faktorial.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD:faktorial.py
 n = int(input("Masukkan nilai : "))
 faktorial = 1
 
 for i in range (1, n +1):
     faktorial *=i
     
-=======
-n = int(input("Masukkan nilai : "))
-faktorial = 1
-
-for i in range (1, n +1):
-    faktorial *=i
-    
->>>>>>> f363c0c524fc02589f7fb6e0fc6af06df1568914:Latihan Python/faktorial.py
 print(f'{n}! = {faktorial}')
